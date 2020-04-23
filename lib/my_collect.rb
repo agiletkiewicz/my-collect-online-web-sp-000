05cc3df24ae8b48455ac822a65dacf6b4bf491b4
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-23 20:06:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,11 @@
 def my_collect(array)
-  i = 0 
-  new_collection=[]
-  while i < array.length 
-  yield(array[i])
+  if block_given?
+    i = 0 
+    new_collection=[]
+    while i < array.length 
+    yield(array[i])
+    end
+  else puts "No block!"
   end
 new_collection
 end
